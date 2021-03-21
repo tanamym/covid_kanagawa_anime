69a4db9af21dac6a97a841a9aef214a27b1a5656
tanamym <66995058+tanamym@users.noreply.github.com>
2021-03-21 16:15:29 +0900

    コード及びデータの変更

--- server.R
+++ server.R
@@ -1,11 +1,4 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 if (!require(htmltools)) {
   install.packages("htmltools")
 }
@@ -52,54 +45,93 @@ webshot :: install_phantomjs()
 
 
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output, session) {
     #コロナのデータ読み込み
-    #data<-fread("https://dl.dropboxusercontent.com/s/6mztoeb6xf78g5w/COVID-19.csv", encoding="UTF-8")
-    #修正済みデータの読み込み
-    #data<-fread("kanagawa.csv", encoding="UTF-8")
-    data<-fread("kanagawa.csv", encoding="UTF-8") %>%
-      mutate(確定日= as.Date(確定日,format = "%m/%d/%Y"))
-    patient<-
-      read.csv("https://www.pref.kanagawa.jp/osirase/1369/data/csv/patient.csv") %>%
-      filter(!str_detect(居住地,"管内")) %>%
-      filter(発表日>="2020-12-01") %>%
-      rename("確定日"="発表日","居住市区町村"="居住地") %>%
-      mutate(年代 = str_replace(年代,"代","")) %>%
-      mutate(居住市区町村 = str_replace(居住市区町村,"神奈川県",""))
-    kanagawa<-read.csv("kanagawa2.csv") %>%
-      select(-X,-備考)#%>%
-      # filter(!居住市区町村%in%c("横浜市","横須賀市","相模原市","川崎市",
-      #                     "藤沢市"))
-    kanagawa2<-rbind(kanagawa,patient) %>%
-      mutate(受診都道府県 ="神奈川県",
-                   居住都道府県="神奈川県"
-      )
-    
-    xy<-read.csv("xy.csv") %>%
-      select(-X.1)
-    chigasaki<-
-      read.csv("chigasaki.csv")%>%
-      mutate(受診都道府県 ="神奈川県",
-                   居住都道府県="神奈川県"
-      )%>%
-      mutate(確定日=as.Date(確定日))%>%
-      left_join(xy,by="居住市区町村")
-    kanagawa2<-
-      left_join(kanagawa2,xy,by="居住市区町村") %>%
-      mutate(確定日=as.Date(確定日))
-    kawasaki<-
-      read.csv("kawasaki.csv") %>%
-      select(-X,番号,番号2)%>%
-      mutate(確定日=as.Date(確定日))
-    
-    data<-bind_rows(data,kanagawa2,kawasaki,chigasaki)
+  data<-
+    fread("kanagawa.csv", encoding="UTF-8") %>%
+    mutate(確定日= as.Date(確定日,format = "%m/%d/%Y"))%>%
+    select(確定日,受診都道府県,居住都道府県,居住市区町村,備考,X,Y)%>%
+    filter(居住都道府県=="神奈川県")%>%
+    filter(居住市区町村=="川崎市"|居住市区町村=="")%>%
+    mutate(Residential_City=paste0(居住市区町村,備考))%>%
+    select(-居住市区町村,-備考,-X,-Y)%>%
+    rename("Fixed_Date"="確定日",
+           "Hospital_Pref"="受診都道府県",
+           "Residential_Pref"="居住都道府県")%>%
+    filter(Residential_City!="")
+  data2<-fread("kanagawa.csv", encoding="UTF-8") %>%
+    mutate(確定日= as.Date(確定日,format = "%m/%d/%Y"))%>%
+    select(確定日,受診都道府県,居住都道府県,居住市区町村,備考,X,Y)%>%
+    filter(居住都道府県=="神奈川県")%>%
+    filter(居住市区町村!="川崎市",居住市区町村!="")%>%
+    rename("Residential_City"="居住市区町村")%>%
+    select(-備考)%>%
+    rename("Fixed_Date"="確定日",
+           "Hospital_Pref"="受診都道府県",
+           "Residential_Pref"="居住都道府県")
+
+  patient<-
+    read.csv("https://www.pref.kanagawa.jp/osirase/1369/data/csv/patient.csv") %>%
+    filter(!str_detect(居住地,"管内")) %>%
+    filter(発表日>="2020-12-01") %>%
+    rename("Fixed_Date"="発表日","Residential_City"="居住地") %>%
+    select(-年代,-性別)%>%
+    mutate(Residential_City = str_replace(Residential_City,"神奈川県",""))%>%
+    mutate(Fixed_Date=as.Date(Fixed_Date))
+
+  kanagawa<-read.csv("kanagawa2.csv") %>%
+    select(-X,-note)%>%
+    #select(-X,-備考,-年代,-性別)%>%
+    #rename("Fixed_Date"="確定日","Residential_City"="居住市区町村")%>%
+    mutate(Fixed_Date=as.Date(Fixed_Date))
+
+  kanagawa2<-rbind(kanagawa,patient) %>%
+    mutate(Hospital_Pref ="神奈川県",
+           Residential_Pref="神奈川県"
+    )
+  
+  xy<-read.csv("xy.csv") %>%
+    select(-X.1)%>%
+    rename("Residential_City"="居住市区町村")
+  chigasaki<-
+    read.csv("chigasaki.csv")%>%
+    mutate(Hospital_Pref ="神奈川県",
+           Residential_Pref="神奈川県"
+    )%>%
+    mutate(Fixed_Date=as.Date(Fixed_Date))%>%
+    left_join(xy,by="Residential_City")%>%
+    filter(!is.na(X))
+
+
+  list1<-read.csv("list.csv")
+  data3<-
+    data%>%
+    left_join(list1,by=c("Residential_City"="list"))%>%
+    select(-Residential_City)%>%
+    rename("Residential_City"="管内")%>%
+    filter(!is.na(X))
+
+  kanagawa2<-
+    left_join(kanagawa2,xy,by="Residential_City") %>%
+    mutate(Fixed_Date=as.Date(Fixed_Date))%>%
+    filter(!is.na(X))
+
+  kawasaki<-
+    read.csv("kawasaki.csv") %>%
+    select(-X)%>%
+    mutate(Fixed_Date=as.Date(Fixed_Date))%>%
+    left_join(list1)%>%
+    select(-note,-管内,-Residential_City)%>%
+    rename("Residential_City"="list")
+
+  data7<-bind_rows(data2,data3,kanagawa2,kawasaki,chigasaki)
     
     date<-
       kawasaki%>%
       data.frame()%>%
-      arrange(desc(確定日))%>%
-      distinct(確定日)
+      arrange(desc(Fixed_Date))%>%
+      distinct(Fixed_Date)
     output$date1<-
       renderUI({
         dateInput("z1",
@@ -112,72 +144,7 @@ shinyServer(function(input, output, session) {
                   label = h5("アニメーションの終了日を入力"),
                   max = date[1,1],value = date[1,1])
       })
-    #data$確定日 <- lubridate::mdy(data$確定日)
-    data$発症日 <- lubridate::mdy(data$発症日)
-    data1<-data%>%
-        select(年代,性別,確定日,発症日,受診都道府県,
-                 居住都道府県,居住管内,居住市区町村,備考,X,Y)%>%
-        filter(居住都道府県=="神奈川県")
-
-
-    data2<-data%>%
-        select(年代,性別,確定日,発症日,受診都道府県,
-                 居住都道府県,居住市区町村,備考)%>%
-        filter(居住都道府県=="神奈川県")
-    list1<-read.csv("list.csv")
-    #無理やり結合させたいので
-    #川崎市備考NAあり
-    #居住市区町村NAの場合に結合
-    data3<-data2%>%
-        filter(居住市区町村=="")
-    list1$list<-as.character(list1$list)
-    data5.1<-inner_join(data3,list1,by=c("備考"="list"))
-    #川崎市はNAがあるため、埋まっている部分だけ結合
-    data4<-data2%>%
-        filter(居住市区町村=="川崎市",備考!="")
-    data5.2<-inner_join(data4,list1,by=c("備考"="list"))
-    #それ以外のデータ
-    #川崎市
-    data5.3<-data1%>%
-        filter(居住市区町村=="川崎市",備考=="")
-    #居住市区町村NAでの備考のリストに当てはまらない部分
-    data5.4<-data1%>%
-        filter(居住市区町村=="")%>%
-        anti_join(list1,by=c("備考"="list"))
-    #居住市区町村が川崎市以外で""ではない市区町村
-    data5.5<-data1%>%
-        filter(居住市区町村!=""&居住市区町村!="川崎市")
-    #川崎市NA
-    data5.6<-data1%>%
-      filter(居住市区町村=="川崎市",is.na(備考))
-    #data5.1~data5.5まで結合
     
-    data6<-bind_rows(data5.1,data5.2,data5.3,data5.4,data5.5,data5.6)
-    #data5.1~data5.5まで結合
-    #data6<-bind_rows(data5.1,data5.2,data5.3,data5.4,data5.5)
-    #居住市区町村と備考と管内を一つにまとめたい
-    #居住市区町村""
-    data6.1<-data6%>%
-        filter(居住市区町村=="")%>%
-        mutate(居住市区町村及び管内=管内)%>%
-        select(年代,性別,確定日,発症日,受診都道府県,
-                 居住都道府県,居住市区町村及び管内,X,Y)
-    #川崎市
-    data6.2<-data6%>%
-        filter(居住市区町村=="川崎市")%>%
-        tidyr::unite(col=居住市区町村及び管内,居住市区町村,管内,sep="",remove=T)%>%
-        select(年代,性別,確定日,発症日,受診都道府県,
-                 居住都道府県,居住市区町村及び管内,X,Y)
-    #川崎市以外で居住市区町村があるところ
-
-    data6.3<-data6%>%
-        filter(居住市区町村!="",居住市区町村!="川崎市")%>%
-        mutate(居住市区町村及び管内=居住市区町村)%>%
-        select(年代,性別,確定日,発症日,受診都道府県,
-                 居住都道府県,居住市区町村及び管内,X,Y)
-    data6.1$居住市区町村及び管内<-as.character(data6.1$居住市区町村及び管内)
-    #結合
-    data7<-dplyr::bind_rows(data6.1, data6.2,data6.3)
 
     jinko<-read.csv("jinko.csv")
     jinko<-data.frame(jinko)
@@ -185,20 +152,20 @@ shinyServer(function(input, output, session) {
     l1=function(a,b){
        #集計
        data7.1<-data7%>%
-         filter(確定日>=a,確定日<=b)%>%
-         group_by(居住市区町村及び管内,X,Y)%>%
+         filter(Fixed_Date>=a,Fixed_Date<=b)%>%
+         group_by(Residential_City,X,Y)%>%
          summarise(count=n())%>%
          filter(X>0,Y>0)
-       jinko2<-left_join(data7.1,jinko,by=c("居住市区町村及び管内"="市区町村"))
+       jinko2<-left_join(data7.1,jinko,by=c("Residential_City"="City"))
        jinko3<-jinko2%>%
-         mutate(count_j=count/人口*100000)
+         mutate(count_j=count/jinko*100000)
        leaflet(jinko3) %>% addTiles() %>%
          addProviderTiles(providers$CartoDB.Positron) %>%
          #setView(lng=139.4025,lat=35.4478,zoom=10)%>%
          fitBounds(lng1=139.124343, lat1=35.117843, lng2=139.652899, lat2=35.665052)%>%
          addCircleMarkers(~X, ~Y, stroke=FALSE,
                           radius =sqrt(jinko3$count_j)*input$en,
-                          label = ~htmlEscape(居住市区町村及び管内),
+                          label = ~htmlEscape(Residential_City),
                           labelOptions = labelOptions(direction = 'auto',noHide = T, textOnly = TRUE,textsize = "10px"))%>%
          addCircleMarkers(~X, ~Y, stroke=FALSE,
                           radius =sqrt(jinko3$count_j)*input$en,
@@ -226,9 +193,7 @@ shinyServer(function(input, output, session) {
       }
       
      file_names <- list.files(pattern = "map_\\d+.png$", full.names = TRUE)
-     # image_read(file_names) %>%
-     #   image_animate(fps = 1) %>%
-     #   image_write(paste0(input$text,"output.gif"))
+
      image_read(file_names) %>%
        image_animate(fps = 1) %>%
        image_write("output.gif")
@@ -271,9 +236,6 @@ shinyServer(function(input, output, session) {
      }
      
    )
-   output$text<-
-     renderText({
-       as.numeric(lubridate::ymd(input$z2)-lubridate::ymd(input$z1))%/%input$x
-     })
+
    
 })
--- ui.R
+++ ui.R
@@ -1,11 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 if (!require(shiny)) {
   install.packages("shiny")
@@ -18,27 +10,19 @@ if (!require(leaflet)) {
 }
 library(leaflet)
 
-#tmp.enc <- options()$encoding #標準コーディングを記録（native.encであることが多いです）
-#options(encoding = "UTF-8") #エンコーディングをUTF-8に変更
-#deployApp()
-#options(encoding = tmp.enc) #エンコーディングをもとに戻す
-# Define UI for application that draws a histogram
+
 shinyUI(fluidPage(
 # Application title
     titlePanel("COVID-MAP"),
     
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
                      h4("アニメーションの設定"),
                      uiOutput("date1"),
                      uiOutput("date2"),
-                     #dateInput("z",label = h5("アニメーションが終わる日付を入力"),max = date[1,1],value = date[1,1]),
                      numericInput("w",label = h5("累積日数を入力"),value="7"),
                      numericInput("x",label = h5("アニメーションの日数の間隔を入力"),value="3"),
                      numericInput("en",label = h5("円の大きさを指定"),value="10"),
-                     #textInput("text",label = h5("保存先を入力してください"),value="C:/"),
-                     #h6("注意:保存先の最後には/を入力してください。また\\ではなく/を入力してください。"),
                      actionButton("submit", "描画"),
                      downloadButton('downloadData', 'GIF Download')
                     
@@ -49,9 +33,7 @@ shinyUI(fluidPage(
         
         # Show a plot of the generated distribution
         mainPanel(
-            h5("アニメーション"),
-            imageOutput("anime"),
-            textOutput("text"),
+            imageOutput("anime")
         )
     )
 
